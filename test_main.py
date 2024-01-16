8c6649c544343248141dd918144d2c4a2098351b
senvisito <125932786+senvisito@users.noreply.github.com>
2024-01-16 18:34:19 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -23,12 +23,10 @@ def test_check_http_status_failure():
     assert monitor.check_http_status("http://invalid-url.com/") == False
 
 
-@pytest_asyncio.fixture
+@pytest.mark.asyncio
 async def test_enviar_mensaje_telegram():
     token = "6972501031:AAFi8k3AqAU0mjFiFg7CrreH5XPwZPAFabM"
     chat_id = "6424284777"
     mensaje = "Prueba de funcionamiento"
     result = await monitor.enviar_mensaje_telegram(token, chat_id, mensaje)
     assert result is None
-    asyncio.run (test_enviar_mensaje_telegram())
-
